5943315413a09f8a9a658ce316a80f1b9a9398ff
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-03-06 08:10:51 +0100

    installed all libraries required for the project

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 social media platform - Twitter, Facebook, Instagram.
 language - react(created with vite), tailwind
-libraries - react-chart-js, redux toolkit
+libraries - react-chartjs-2, redux toolkit
 icons - react icons
 authentication - firebase
-api - 
+api - fetching with axios
 hosting - vercel
---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCK_QwWXEh1jfk1lDLuiHl5_KXWGbLPRo",
+  authDomain: "analyza-83a2d.firebaseapp.com",
+  projectId: "analyza-83a2d",
+  storageBucket: "analyza-83a2d.appspot.com",
+  messagingSenderId: "328725374760",
+  appId: "1:328725374760:web:de64f7592644716427921d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
