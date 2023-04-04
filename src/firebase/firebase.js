f33b5b6c5a91ec2d00530eb0b60c585311e4ca1a
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-04-04 22:05:16 +0100

    minor ui changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBCK_QwWXEh1jfk1lDLuiHl5_KXWGbLPRo",
   authDomain: "analyza-83a2d.firebaseapp.com",
@@ -13,5 +10,4 @@ const firebaseConfig = {
   appId: "1:328725374760:web:de64f7592644716427921d",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/store/Reducers/authSlice.js
+++ src/store/Reducers/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  userToken: "",
+  userToken: null,
 };
 
 export const authSlice = createSlice({
